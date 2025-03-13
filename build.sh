a0571a02bc28b1b3f61f2aad52ecc8363bb5e4cc
Alex Siegel <alexsiegel@Mac.hsd1.fl.comcast.net>
2025-03-13 08:55:35 -0700

    Fix deployment build issues

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 cd frontend
 
 # Install dependencies with legacy peer deps
-npm ci --legacy-peer-deps
+npm install --legacy-peer-deps
 
 # Install ajv, ajv-keywords, and CRACO explicitly to fix the build issue
-npm install ajv@8.12.0 ajv-keywords@5.1.0 @craco/craco@7.1.0 --legacy-peer-deps
+npm install ajv@8.12.0 ajv-keywords@5.1.0 @craco/craco@7.1.0 cosmiconfig-typescript-loader@1.0.9 --legacy-peer-deps
 
 # Build the frontend
 npm run build
