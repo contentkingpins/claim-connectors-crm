79253f0ad3c7d1b53fc5c9e88483a8b723eeddf3
Alex Siegel <alexsiegel@Mac.hsd1.fl.comcast.net>
2025-03-13 08:41:01 -0700

    Fix build issues with ajv dependency

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ cd frontend
 # Install dependencies with legacy peer deps
 npm ci --legacy-peer-deps
 
+# Install ajv explicitly to fix the build issue
+npm install ajv@6.12.6 --no-save --legacy-peer-deps
+
 # Build the frontend
 npm run build
 
